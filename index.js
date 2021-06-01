8165c902687eb2fc2d13da8f59b077b300bceb99
valeponchi <81304855+valeponchi@users.noreply.github.com>
2021-06-01 12:38:09 +0100

    Co-authored-by: ultrahumus9000 <ultrahumus9000@users.noreply.github.com>

---
+++ index.js
@@ -0,0 +1,44 @@
+let state = {
+  users: 
+    [
+      {
+        id: 1,
+        name: Valentina,
+      },
+      {
+        id: 2,
+        name:Linlin,
+      }
+    ],
+  post:
+    [
+      { 
+        userId: 1,
+        postTitle: "My favourite Anime",
+        image: "",
+        genre: "anime",
+        comment: "I really loved FullMetal Alchimist",
+        animeInfo: ""
+      },
+      { 
+        userId: 1,
+        postTitle: "",
+        image: "",
+        genre: "",
+        comment: "",
+        animeInfo: {
+          movie
+        }
+      },
+      { 
+        userId: 2,
+        postTitle: "",
+        image: "",
+        genre: "",
+        comment: "",
+        animeInfo: ""
+
+      }
+    ]
+
+}
