e10df17338eee7861213b94bc4218dd09ef4e546
Linlin Li <paris1900lll@gmail.com>
2021-06-01 13:10:42 +0100

    db json setup

--- index.js
+++ index.js
@@ -1,44 +1,29 @@
 let state = {
-  users: 
-    [
-      {
-        id: 1,
-        name: Valentina,
+  users: [
+    {
+      id: 1,
+      name: Valentina,
+    },
+    {
+      id: 2,
+      name: Linlin,
+    },
+  ],
+  posts: [
+    {
+      id: 1,
+      userId: 1,
+      postTitle: "My favourite Anime",
+      image: "",
+      genre: "anime",
+      comment: "I really loved FullMetal Alchimist",
+      animeId: "2baf70d1-42bb-4437-b551-e5fed5a87abe",
+      rating: 1,
+      animeInfo: {
+        title: "",
+        originalTitle: "",
+        director: "",
       },
-      {
-        id: 2,
-        name:Linlin,
-      }
-    ],
-  post:
-    [
-      { 
-        userId: 1,
-        postTitle: "My favourite Anime",
-        image: "",
-        genre: "anime",
-        comment: "I really loved FullMetal Alchimist",
-        animeInfo: ""
-      },
-      { 
-        userId: 1,
-        postTitle: "",
-        image: "",
-        genre: "",
-        comment: "",
-        animeInfo: {
-          movie
-        }
-      },
-      { 
-        userId: 2,
-        postTitle: "",
-        image: "",
-        genre: "",
-        comment: "",
-        animeInfo: ""
-
-      }
-    ]
-
-}
+    },
+  ],
+};
